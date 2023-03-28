305155efd1b9a2f43fd71b6e28f564ed3dea9ed5
Clara <clarapachecodev@gmail.com>
2023-03-28 16:53:49 -0300

    Import image and add it to image's tag src located inside Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import styles from './Header.module.css'
 
+import igniteLogo from '../../assets/ignite-logo.svg'
+
+/* igniteLogo guardará um caminho, e é esse caminho que
+precisamos colocar no source(src) da imagem - 
+como se trata de uma variável, temos que colocar entre { } */
+
 export function Header(){
   return (
     <div className={ styles.header }>
-      <strong>Ignite Feed</strong>
+      <img src= { igniteLogo } alt="Logotipo do Ignite" />
     </div>
 
   )
