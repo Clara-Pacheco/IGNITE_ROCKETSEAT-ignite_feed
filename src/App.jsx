18042a73827b02fcc8fcaa2c869409203257ff53
Clara <clarapachecodev@gmail.com>
2023-03-28 10:56:07 -0300

    Create structure with vite + component folder containing Header component and Header css module

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
+import { Post } from './Post'
+import { Header } from './components/Header'
+
+import './styles.css'
+
 function App() {
 
 
   return (
-    <h1>Hello World</h1>
+    <div>
+      <Header />
+      <h1>Hello World</h1>
+      <Post 
+        author='Clara Pacheco' 
+        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+      />
+
+      <Post
+        author='Emma' 
+        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+      />
+
+      <Post
+        author='Paulo' 
+        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+      />
+    </div>
   )
 }
 
 export default App
+
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post({ author, content }) {
+  return (
+    <div>
+      
+      <h1><strong>{ author }</strong></h1>
+      <h2>{ content }</h2>
+    </div>
+  )
+}
