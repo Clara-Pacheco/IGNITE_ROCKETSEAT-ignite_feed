07f0c4dc4dc8f3b11476d283f4bb8e9406e5dfa9
Clara <clarapachecodev@gmail.com>
2023-03-29 08:54:50 -0300

    Import App module css file and create the structure to contain the Sidebar component + main feed section containing Post components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import { Post } from './Post'
 import { Header } from './components/Header'
 
+// Import CSS GLOBAL
 import './global.css'
+
+// IMPORT APP MODULE CSS
+import styles from './App.module.css'
+import { Sidebar } from './components/Sidebar'
 
 function App() {
 
@@ -9,21 +14,32 @@ function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+       
+       <Sidebar />
+
+        <main>
+            <Post 
+            author='Clara Pacheco' 
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+          />
+
+          <Post
+            author='Emma' 
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+          />
+
+          <Post
+            author='Paulo' 
+            content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
+          />
+        </main>
+
+
+      </div>
      
-      <Post 
-        author='Clara Pacheco' 
-        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
-      />
-
-      <Post
-        author='Emma' 
-        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
-      />
-
-      <Post
-        author='Paulo' 
-        content='Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam, nam sit. Blanditiis neque nesciunt enim, quod at praesentium. Cupiditate, laudantium facilis voluptatem porro beatae iure. Nisi repudiandae provident quidem debitis?'
-      />
+      
     </div>
   )
 }
