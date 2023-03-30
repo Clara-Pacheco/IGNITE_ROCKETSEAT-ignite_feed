55fe5e3d1424cc56e44a99a0f520b925ab198494
Clara <clarapachecodev@gmail.com>
2023-03-30 16:04:39 -0300

    Substitute img element for Avatar component passing hasBorder and src properties

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,14 @@
 import styles from './Post.module.css'
 
 import { Comment } from './Comment'
+import { Avatar } from './Avatar'
 
 export function Post() {
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img
-           className={styles.avatar}
-           src='https://avatars.githubusercontent.com/u/93992748?v=4' 
-          />
+          <Avatar hasBorder src='https://avatars.githubusercontent.com/u/93992748?v=4' />
           <div className={styles.authorInfo}>
             <strong>Clara Pacheco</strong>
             <span>Web Developer</span>
