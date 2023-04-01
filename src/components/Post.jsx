9b35816dffed0bcd65bf9d143e3df9742caccfc4
Clara <clarapachecodev@gmail.com>
2023-04-01 13:35:36 -0300

    Create a state variable to hold the changes in textarea, so that those changes are rendered afterwards when passed as parameters to setComments state function

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -15,9 +15,10 @@ import { useState } from 'react'
 export function Post({author,content,publishedAt}) {
 
   const [comments, setComments] = useState([
-    1,
-    2
+    'Post muito bacana,hein!'
   ])
+
+  const [newCommentText, setNewCommentText] = useState('')
 
   // Variável que será atribuída ao parâmetro title da tag <time>
   const publishedDateFormatted = format(publishedAt,"d 'de' LLLL 'às' HH:mm'h'", {
@@ -39,7 +40,13 @@ export function Post({author,content,publishedAt}) {
 
   function handleCreateNewComment(){
     event.preventDefault()
-    setComments([...comments, comments.length + 1])
+    
+    setComments([...comments, newCommentText])
+    setNewCommentText('')
+  }
+
+  function handleNewCommentChange() {
+    setNewCommentText(event.target.value)
   }
 
   return (
@@ -72,7 +79,12 @@ export function Post({author,content,publishedAt}) {
       <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe o seu feedback</strong>
 
-        <textarea placeholder='Deixe um comentário'/>
+        <textarea
+         name="comment"
+         placeholder='Deixe um comentário'
+         value={newCommentText}
+         onChange={handleNewCommentChange}
+         />
 
         <footer>
           <button type='submit'>Publicar</button>
@@ -81,7 +93,7 @@ export function Post({author,content,publishedAt}) {
 
       <div className={styles.commentList}>
        {comments.map(comment=>{
-        return <Comment />
+        return <Comment content={comment} />
        })}
       </div>
     </article>
