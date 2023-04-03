98434703ad87ab3ce7e39d8caa81624f5f1d4800
Clara <clarapachecodev@gmail.com>
2023-04-03 07:49:42 -0300

    Create a function inside Comment component, to handle de action to delete comments and, inside of it, call the function received as argument deleteComment, passing the content of the comment.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -9,7 +9,13 @@ import { Trash, ThumbsUp } from 'phosphor-react'
 
 import { Avatar } from './Avatar'
 
-export function Comment({content}) {
+
+export function Comment({content, ondeleteComment}) {
+  function handleDeleteComment(){
+    ondeleteComment(content)
+  }
+
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src='https://avatars.githubusercontent.com/u/93992748?v=4' />
@@ -22,7 +28,7 @@ export function Comment({content}) {
               <time title='11 de maio as 8:13h' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
             </div>
 
-            <button title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
